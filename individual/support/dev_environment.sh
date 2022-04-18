1257a729a2f899e10bd6bd8d9069b65e5f9b439c
Michael B. Klein <mbklein@gmail.com>
2022-04-18 15:03:30 -0500

    Add bash script to load environment from SSM

---
+++ individual/support/dev_environment.sh
@@ -0,0 +1,18 @@
+next_page() {
+  cmd="aws ssm get-parameters-by-path --path /dev-environment/ --recursive --with-decryption"
+  if [[ -n $next_token && $next_token != "null" ]]; then
+    cmd="$cmd --next-token $next_token"
+  fi
+  params=$($(echo $cmd))
+  vars=$(jq -r '.Parameters[] | (.Name | sub("^/dev-environment/"; "") | gsub("[/-]"; "_") | ascii_upcase) as $name | "DEV_\($name)=\(.Value)"' <<< $params)
+  export eval $vars
+  next_token=$(jq '.NextToken' <<< $params)
+}
+
+next_token=""
+while [[ $next_token != "null" ]]; do
+  next_page
+done
+
+instance_id=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
+export DEV_PREFIX=$(aws ec2 describe-tags --filters "Name=resource-id,Values=$instance_id" "Name=key,Values=owner" | jq -r '.Tags[].Value')

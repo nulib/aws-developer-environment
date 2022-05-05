ca186a8dd11ba092da303f041102ef35a436e307
Michael B. Klein <mbklein@gmail.com>
2022-05-05 16:37:11 -0500

    Move environment module to the top level

--- environment/main.tf
+++ environment/main.tf
@@ -2,9 +2,7 @@ data "aws_caller_identity" "current_user" {}
 data "aws_region" "current" {}
 
 locals {
-  project                   = "dev-environment"
+  project                   = var.project
   prefix                    = var.name
-  owner                     = split("-", local.prefix)[0]
-  environment               = split("-", local.prefix)[1]
   regional_id               = join(":", [data.aws_region.current.name, data.aws_caller_identity.current_user.id])
 }
--- individual/environments.tf
+++ individual/environments.tf
@@ -1,5 +1,5 @@
 module "environment" {
-  source                = "./environment"
+  source                = "../environment"
   for_each              = toset(var.environments)
   name                  = "${local.owner}-${each.key}"
   fixity_function_arn   = local.common_config.fixity_function_arn

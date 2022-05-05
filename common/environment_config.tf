05362898b76c947fa267bb62bda8dc847c6025e1
Michael B. Klein <mbklein@gmail.com>
2022-05-05 09:06:43 -0500

    Refactor environment config to use fewer secrets

--- common/environment_config.tf
+++ common/environment_config.tf
@@ -1,74 +1,53 @@
-resource "aws_secretsmanager_secret" "db_secrets" {
-  name    = "${local.project}/db"
-  description = "Database configuration secrets"
-}
+locals {
+  computed_secrets = {
+    db   = {
+      host        = module.aurora_postgresql.cluster_endpoint
+      port        = module.aurora_postgresql.cluster_port
+      user        = module.aurora_postgresql.cluster_master_username
+      password    = module.aurora_postgresql.cluster_master_password
+    }
+    index = {
+      index_endpoint    = "https://${aws_opensearch_domain.search_index.endpoint}"
+      kibana_endpoint   = "https://${aws_opensearch_domain.search_index.kibana_endpoint}"
+    }
 
-resource "aws_secretsmanager_secret" "index_secrets" {
-  name    = "${local.project}/index"
-  description = "OpenSearch index secrets"
-}
+    ldap = merge(var.ldap_config, {
+      host = join(".", [aws_service_discovery_service.ldap.name, aws_service_discovery_private_dns_namespace.internal.name])
+    })
 
-resource "aws_secretsmanager_secret" "ldap_secrets" {
-  name    = "${local.project}/ldap"
-  description = "LDAP server secrets"
-}
+    pipeline = {
+      for key in keys(local.pipeline): 
+        key => module.pipeline_lambda[key].lambda_function_qualified_arn
+    }
+  }
 
-resource "aws_secretsmanager_secret" "pipeline_secrets" {
-  name    = "${local.project}/pipeline"
-  description = "Ingest pipeline secrets"
+  config_secrets = merge(
+    var.config_secrets,
+    local.computed_secrets
+  )
+
+  ssl_certificate = {
+    certificate = file(var.ssl_certificate_file)
+    key         = file(var.ssl_key_file)
+  }
 }
 
 resource "aws_secretsmanager_secret" "config_secrets" {
-  name    = "${local.project}/config"
-  description = "Miscellaneous configuration secrets"
+  name    = "${local.project}/config/meadow"
+  description = "Meadow configuration secrets"
 }
 
 resource "aws_secretsmanager_secret" "ssl_certificate" {
-  name = "${local.project}/ssl"
+  name = "${local.project}/config/wildcard_ssl"
   description = "Wildcard SSL certificate and private key"
-}
-
-resource "aws_secretsmanager_secret_version" "db_secrets" {
-  secret_id = aws_secretsmanager_secret.db_secrets.id
-  secret_string = jsonencode({
-    host        = module.aurora_postgresql.cluster_endpoint
-    port        = module.aurora_postgresql.cluster_port
-    user        = module.aurora_postgresql.cluster_master_username
-    password    = module.aurora_postgresql.cluster_master_password
-  })
-}
-
-resource "aws_secretsmanager_secret_version" "index_secrets" {
-  secret_id = aws_secretsmanager_secret.index_secrets.id
-  secret_string = jsonencode({
-    index_endpoint    = "https://${aws_opensearch_domain.search_index.endpoint}"
-    kibana_endpoint   = "https://${aws_opensearch_domain.search_index.kibana_endpoint}"
-  })
-}
-
-resource "aws_secretsmanager_secret_version" "ldap_secrets" {
-  secret_id = aws_secretsmanager_secret.ldap_secrets.id
-  secret_string = jsonencode(merge(var.ldap_config, {
-    host = join(".", [aws_service_discovery_service.ldap.name, aws_service_discovery_private_dns_namespace.internal.name])
-  }))
-}
-
-resource "aws_secretsmanager_secret_version" "pipeline_secrets" {
-  secret_id = aws_secretsmanager_secret.pipeline_secrets.id
-  secret_string = jsonencode(
-    {for key in keys(local.pipeline): key => module.pipeline_lambda[key].lambda_function_qualified_arn}
-  )
 }
 
 resource "aws_secretsmanager_secret_version" "config_secrets" {
   secret_id = aws_secretsmanager_secret.config_secrets.id
-  secret_string = jsonencode(var.config_secrets)
+  secret_string = jsonencode(local.config_secrets)
 }
 
 resource "aws_secretsmanager_secret_version" "ssl_certificate" {
-  secret_id       = aws_secretsmanager_secret.ssl_certificate.id
-  secret_string   = jsonencode({
-    certificate = file(var.ssl_certificate_file)
-    key         = file(var.ssl_key_file)
-  })
+  secret_id = aws_secretsmanager_secret.ssl_certificate.id
+  secret_string = jsonencode(local.ssl_certificate)
 }
--- common/outputs.tf
+++ common/outputs.tf
@@ -1,7 +1,6 @@
 resource "aws_ssm_parameter" "output_parameter" {
   for_each = {
     fixity_function_arn          = module.execute_fixity_function.lambda_function_arn
-    iiif_base_url                = "https://${aws_route53_record.serverless_iiif.name}/iiif/2/"
     vpc_id                       = module.vpc.vpc_id
   }
 

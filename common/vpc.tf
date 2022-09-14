2aae66df521430f9bd445cb0ed4664a8b944c1a4
Michael B. Klein <mbklein@gmail.com>
2022-09-13 23:26:36 -0500

    Fix VPC so ECS can work without a NAT gateway

--- common/vpc.tf
+++ common/vpc.tf
@@ -14,6 +14,15 @@ module "vpc" {
   enable_vpn_gateway      = false
 }
 
+resource "aws_security_group_rule" "default_allows_all_in_vpc" {
+  security_group_id   = module.vpc.default_security_group_id
+  type                = "ingress"
+  from_port           = 0
+  to_port             = 65535
+  protocol            = "all"
+  cidr_blocks         = [module.vpc.vpc_cidr_block]
+}
+
 module "endpoints" {
   source    = "terraform-aws-modules/vpc/aws//modules/vpc-endpoints"
   version   = "~> 3.14"
@@ -23,12 +32,50 @@ module "endpoints" {
 
   endpoints = {
     s3        = { 
-      route_table_ids   = [module.vpc.vpc_main_route_table_id]
+      route_table_ids   = concat([module.vpc.vpc_main_route_table_id], module.vpc.public_route_table_ids, module.vpc.private_route_table_ids)
       service           = "s3"
       service_type      = "Gateway"
     }
-    lambda    = { service = "lambda" }
-    sns       = { service = "sns"    }
-    sqs       = { service = "sqs"    }
+
+    ecr_api   = { 
+      service               = "ecr.api"
+      subnet_ids            = module.vpc.private_subnets
+      private_dns_enabled   = true
+    }
+    ecr_dkr   = { 
+      service               = "ecr.dkr"
+      subnet_ids            = module.vpc.private_subnets
+      private_dns_enabled   = true
+    }
+    lambda    = { 
+      service               = "lambda"
+      subnet_ids            = module.vpc.private_subnets
+      private_dns_enabled   = true
+    }
+    logs      = { 
+      service               = "logs"
+      subnet_ids            = module.vpc.private_subnets
+      private_dns_enabled   = true
+    }
+    secrets   = { 
+      service               = "secretsmanager"
+      subnet_ids            = module.vpc.private_subnets
+      private_dns_enabled   = true
+    }
+    sns       = { 
+      service               = "sns"
+      subnet_ids            = module.vpc.private_subnets
+      private_dns_enabled   = true
+    }
+    sqs       = { 
+      service               = "sqs"
+      subnet_ids            = module.vpc.private_subnets
+      private_dns_enabled   = true
+    }
+    ssm       = { 
+      service               = "ssm"
+      subnet_ids            = module.vpc.private_subnets
+      private_dns_enabled   = true
+    }
   }
 }

169f755124d637be40ca06a6f69060ad8ea062f9
Michael B. Klein <mbklein@gmail.com>
2022-11-03 18:15:38 +0000

    Update Aurora to PostgreSQL 11

--- common/db.tf
+++ common/db.tf
@@ -1,15 +1,17 @@
 module "aurora_postgresql" {
   source  = "terraform-aws-modules/rds-aurora/aws"
-  version = "~> 6.2"
+  version = "~> 7.6"
 
-  name                  = "${local.project}-db-cluster"
-  engine                = "aurora-postgresql"
-  engine_version        = "11.12"
-  engine_mode           = "serverless"
-  vpc_id                = module.vpc.vpc_id
-  subnets               = module.vpc.private_subnets
-  allowed_cidr_blocks   = [module.vpc.vpc_cidr_block]
-  apply_immediately     = true
+  name                          = "${local.project}-db-cluster"
+  engine                        = "aurora-postgresql"
+  engine_version                = "11.13"
+  engine_mode                   = "serverless"
+  vpc_id                        = module.vpc.vpc_id
+  subnets                       = module.vpc.private_subnets
+  allowed_cidr_blocks           = [module.vpc.vpc_cidr_block]
+  allow_major_version_upgrade   = true
+  apply_immediately             = true
+  
   scaling_configuration = {
     auto_pause               = true
     max_capacity             = 32

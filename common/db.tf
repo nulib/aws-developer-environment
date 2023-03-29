5071636870daffdeb64251b825ccc5634f723f98
Michael B. Klein <mbklein@gmail.com>
2023-03-29 17:53:25 +0000

    wip

--- common/db.tf
+++ common/db.tf
@@ -1,23 +1,35 @@
 module "aurora_postgresql" {
   source  = "terraform-aws-modules/rds-aurora/aws"
-  version = "~> 7.6"
+  version = "~> 7.7"
 
   name                          = "${local.project}-db-cluster"
   engine                        = "aurora-postgresql"
-  engine_version                = "11.13"
-  engine_mode                   = "serverless"
+  engine_version                = "14.6"
+  engine_mode                   = "provisioned"
   vpc_id                        = module.vpc.vpc_id
   subnets                       = module.vpc.private_subnets
   allowed_cidr_blocks           = [module.vpc.vpc_cidr_block]
   allow_major_version_upgrade   = true
   apply_immediately             = true
-  
-  scaling_configuration = {
-    auto_pause               = true
-    max_capacity             = 32
-    min_capacity             = 2
-    seconds_until_auto_pause = 3600
-    timeout_action           = "ForceApplyCapacityChange"
+  create_db_parameter_group     = true
+
+  serverlessv2_scaling_configuration = {
+    min_capacity = 0.5
+    max_capacity = 16
   }
+
+  instance_class                = "db.serverless"
+  instances = {
+    one = {}
+  }
+
+  db_parameter_group_family     = "aurora-postgresql14"
+  db_parameter_group_parameters = [ 
+    {
+      name            = "max_locks_per_transaction "
+      value           = 2048
+      apply_method    = "pending-reboot"
+    }
+  ]
 }
 

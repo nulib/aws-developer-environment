6d40ea034ed9a892e63a8ab9062afa189a979bbe
Michael B. Klein <mbklein@gmail.com>
2025-02-17 23:11:54 +0000

    Enable logical replication on DB

--- common/db.tf
+++ common/db.tf
@@ -2,17 +2,18 @@ module "aurora_postgresql" {
   source  = "terraform-aws-modules/rds-aurora/aws"
   version = "~> 7.7"
 
-  name                          = "${local.project}-db-cluster"
-  engine                        = "aurora-postgresql"
-  engine_version                = "16.1"
-  engine_mode                   = "provisioned"
-  vpc_id                        = module.vpc.vpc_id
-  subnets                       = module.vpc.public_subnets
-  allowed_cidr_blocks           = [module.vpc.vpc_cidr_block]
-  allow_major_version_upgrade   = true
-  apply_immediately             = true
-  create_db_parameter_group     = false
-  enable_http_endpoint          = true
+  name                                = "${local.project}-db-cluster"
+  engine                              = "aurora-postgresql"
+  engine_version                      = "16.1"
+  engine_mode                         = "provisioned"
+  vpc_id                              = module.vpc.vpc_id
+  subnets                             = module.vpc.public_subnets
+  allowed_cidr_blocks                 = [module.vpc.vpc_cidr_block]
+  allow_major_version_upgrade         = true
+  apply_immediately                   = true
+  create_db_cluster_parameter_group   = true
+  create_db_parameter_group           = true
+  enable_http_endpoint                = true
 
   serverlessv2_scaling_configuration = {
     min_capacity = 0.5
@@ -24,8 +25,22 @@ module "aurora_postgresql" {
     one = {}
   }
 
+  db_cluster_parameter_group_family     = "aurora-postgresql16"
+  db_cluster_parameter_group_parameters = [
+    {
+      name            = "rds.logical_replication"
+      value           = 1
+      apply_method    = "pending-reboot"
+    },
+  ]
+
   db_parameter_group_family     = "aurora-postgresql16"
-  db_parameter_group_parameters = [ 
+  db_parameter_group_parameters = [
+    {
+      name            = "max_locks_per_transaction"
+      value           = 1024
+      apply_method    = "pending-reboot"
+    }
   ]
 }
 
